e897ab174aa76dbde4dad3920024192e86185026
Jose Manuel <josgiort@hotmail.com>
2024-09-09 20:57:02 +0200

    MakeChart.py version 1

--- MakeChart.py
+++ MakeChart.py
@@ -1,38 +1,41 @@
 import re
+import matplotlib.pyplot as plt
+import numpy as np
 
-file1 = open('chart_info_2.txt', 'r')
-count = 0
+file1 = open('plot_info_2.txt', 'r')
 list_length = []
 list_emb_score = []
-
 while True:
-    #count += 1
-
     line = file1.readline()
     x = re.findall("\t[0-9]+\t", line)
-    y = re.findall("\t[0-9]+,[0-9]+(?:e-[0-9][0-9])?\t", line)
+    y = re.findall("\t[0-9]+,[0-9]+(?:E-[0-9][0-9])?\t", line)
     # Verify if length is exactly 1 in both regex to maintain proper correspondence between lengths and scores
     if len(x) == 1 and len(y) == 1:
         x = x[0].strip()
-        list_length.append(x)
+        list_length.append(int(x))
         y = y[0].strip()
-        list_emb_score.append(y)
+        list_emb_score.append(float(y.replace(',', '.')))
     #else:
         # print("some strange behavior in text file, check it!")
         # exit(0)
-
     if not line:
         break
-    #print("Line{}: {}".format(count, line.strip()))
-
 file1.close()
 
-for i in range(0, len(list_length)):
-    print("length: " + list_length[i] + ", emb score: " + list_emb_score[i])
+x = list_length
+y = list_emb_score
 
+coefficients = np.polyfit(x, y, 1)
+regression_line = np.poly1d(coefficients)
 
-#line = "seq1:1185-1215  30  8,452065230812877e-06   negative"
-#line = "seq1:1185-1215  30  8,452065230812877   negative"
+plt.scatter(x, y,  s=14, alpha=1.0, edgecolors="k")
+plt.plot(x, regression_line(x))
+#plt. axhline(y=0.5, color='r', linestyle='--', linewidth=1)
+#plt.xticks(range(0,1023,50), range(0,1023,50), rotation=35)
+plt.xticks(range(0,1023,50), range(0,1023,50), rotation=35)
 
-#y = re.findall(" [0-9]+,[0-9]+(?:e-[0-9][0-9])? ", line)
-#print(y)
+plt.xlabel('RNA length segment')
+plt.ylabel('Interaction probability')
+plt.title("Interaction prediction")
+plt.legend()
+plt.show()
